f93ce295fe1037ae47da356e7a76374fb225d5da
AlvarBer <alvaro.garcia95@hotmail.com>
2016-01-30 00:17:42 +0100

    Added Player Movement System

---
+++ Assets/Code/Movement/RegularInput.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+abstract public class RegularInput : MonoBehaviour {
+	// Attributes
+	public float speed = 1;
+	public float jumpHeight = 1;
+
+	private float vMov;
+	private float hMov;
+
+	// Functions
+	private void Start() {
+		getControl();
+	}
+
+	private void Update() {
+		hMov = Input.GetAxis("Horizontal");
+		vMov = Input.GetAxis("Vertical");
+		
+		move(hMov, vMov);
+		if (Input.GetButtonDown("Jump")) {
+			jump();
+		}
+		else if (Input.GetButton("Jump")) {
+			extraJump();
+		}
+	}
+
+	abstract public void move(float vMov, float hMov);
+	abstract public void jump();
+	abstract public void getControl();
+	abstract public void extraJump();
+}
---
+++ Assets/Code/Movement/RegularInput2D.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody2D))]
+public class RegularInput2D : RegularInput {
+	private Rigidbody2D body;
+	private bool grounded = false;
+	private float lastJumpTime = 0f;
+
+	override public void move(float hMov, float vMov) {
+		body.AddForce(new Vector2(hMov * speed, 0));
+	}
+
+	override public void jump() {
+		if (grounded) {
+			body.AddForce(Vector2.up * jumpHeight);
+			grounded = false;
+			lastJumpTime = Time.time;
+		}			
+	}
+
+	override public void extraJump() {
+			if (Time.time - lastJumpTime < .09)
+				body.AddForce(Vector2.up * jumpHeight/3);
+	}
+
+
+	override public void getControl() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll) {
+		if(coll.gameObject.name == "Floor")
+			grounded = true;
+	}
+}
