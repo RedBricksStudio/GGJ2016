9700108072c95997f05fc4d83572e074f97a4a7c
pegartillo95 <pegarc03@ucm.es>
2016-01-30 13:11:42 +0100

    Enemy patrol and waypoints

---
+++ Assets/Code/Collider Actions/ChangeToFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeToFollow : MonoBehaviour {
+	public GameObject Enemy;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			DumbEnemyAI player_script = Enemy.GetComponent<DumbEnemyAI>();
+			player_script.playerInVision = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			DumbEnemyAI player_script = Enemy.GetComponent<DumbEnemyAI>();
+			player_script.playerInVision = false;
+		}
+	}
+}
---
+++ Assets/Code/Collider Actions/ChangeWaypoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeWaypoint : MonoBehaviour {
+	public GameObject Enemy;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Enemy") {
+			DumbEnemyAI player_script = Enemy.GetComponent<DumbEnemyAI>();
+			player_script.m_waypointReached = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			DumbEnemyAI player_script = Enemy.GetComponent<DumbEnemyAI>();
+			player_script.m_waypointReached = false;
+		}
+	}
+}
