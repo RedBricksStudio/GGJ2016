1d0ab2d1a5faa82a3c1a4baf574b2255c571a25c
AlvarBer <alvaro.garcia95@hotmail.com>
2016-01-30 21:52:11 +0100

    Player Death now sends him to GameOver

--- Assets/Code/EndConditions/KillPlayerOnContact.cs
+++ Assets/Code/EndConditions/KillPlayerOnContact.cs
@@ -17,8 +17,7 @@ public class KillPlayerOnContact : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
-        if (coll.gameObject.tag == "Player") {
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-		}       
-    }
+		if (coll.gameObject.tag == "Player")
+			SceneManager.LoadScene("GameOver");
+	}
 }
