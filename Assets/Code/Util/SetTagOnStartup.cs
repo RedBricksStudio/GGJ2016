bd905731b62661533182b5359678881b68325e51
blorente <blorente@ucm.es>
2016-01-30 10:48:42 +0100

    Praise the sun!!

---
+++ Assets/Code/Util/SetTagOnStartup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetTagOnStartup : MonoBehaviour {
+
+	public string startingTag;
+
+	// Use this for initialization
+	void Awake () {
+		this.tag = startingTag;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
